adceb5a60a6968d288c397e9e9894ce818d29dfb
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-19 08:16:37 +0530

    Update india-budget.py

--- india-budget.py
+++ india-budget.py
@@ -63,6 +63,6 @@ df = df.sort_values(by='Date')
 # Convert Date column to string without time
 df['Date_str'] = df['Date'].dt.strftime('%d-%m-%Y')
 
-df = df.sort_values("Date", ascending = False)
+df = df.sort_values("Date", ascending = False).reset_index(drop=True)
 
 st.write(df)
