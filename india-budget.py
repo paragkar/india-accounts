adc26a2e1e6d160246f05337dc50f9ac9d72ba28
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-19 13:18:02 +0530

    Update india-budget.py

--- india-budget.py
+++ india-budget.py
@@ -84,6 +84,9 @@ df = df.sort_values(by=['Date', 'Description'], ascending=[False, False])
 
 df["Actual % of BE"] = ((df["Actual"].astype(float)/df["BE"].astype(float))*100).round(2)
 
+df["Actual"] = df["Actual"].astype(float)/100000 #converting into Rs Lakk Cr
+df["BE"] = df["BE"].astype(float)/100000 #converting into Rs Lakk Cr
+
 # Unique dates sorted
 unique_dates = df['Date'].unique()
 date_index = range(len(unique_dates))
