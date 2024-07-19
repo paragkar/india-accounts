fc58aefbc166b48cbea84f8b464f8086f3f8ffb1
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-19 10:19:25 +0530

    Update india-budget.py

--- india-budget.py
+++ india-budget.py
@@ -127,5 +127,9 @@ fig.update_layout(
 # Update the y-axis tick labels to be bold
 fig.update_yaxes(tickfont=dict(size=15, family='Arial', color='black', weight='bold'), row=1, col=1)
 
+# Update the layout for the combined figure
+fig.update_xaxes(row=1, col=1, range=[overall_min_value, overall_max_value * 1.05], fixedrange=True, showline=True, linewidth=1.5, linecolor='grey', mirror=True, showgrid=True, gridcolor='lightgrey')
+fig.update_yaxes(row=1, col=1, tickfont=dict(size=15),fixedrange=True, showline=True, linewidth=1.5, linecolor='grey', mirror=True, showgrid=True, gridcolor='lightgrey')
+
 # Display plot
 st.plotly_chart(fig)
