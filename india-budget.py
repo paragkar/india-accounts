3945d9a68ce435560e41bbd922682c367b0586f6
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-19 06:58:19 +0530

    Create india-budget.py

---
+++ india-budget.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from datetime import datetime
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import streamlit as st
+import io
+import msoffcrypto
+import numpy as np
+import re
+import time
+
+pd.set_option('future.no_silent_downcasting', True)
+pd.set_option('display.max_columns', None)
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.markdown("""
+    <style>
+        .stMultiSelect [data-baseweb=select] span{
+            max-width: 250px;
+            font-size: 0.7rem;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Hide Streamlit style and buttons
+hide_st_style = '''
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+'''
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# Load file function
+@st.cache_data
+def loadfile():
+    password = st.secrets["db_password"]
+    excel_content = io.BytesIO()
+    with open("T01_Main.xlsx", 'rb') as f:
+        excel = msoffcrypto.OfficeFile(f)
+        excel.load_key(password)
+        excel.decrypt(excel_content)
+    
+    # Loading data from excel file
+    df = pd.read_excel(excel_content, sheet_name="Sheet1")
+    return df
+
+# Main Program Starts Here
+df = loadfile()
+
+st.write(df)
